ea60bb736615e5f3e4f323e1c278311a13df5883
JyaGlbert <jiyajiya8812@gmail.com>
2024-11-04 16:03:13 +0530

    4nov

---
+++ demo.py
@@ -0,0 +1,169 @@
+
+# print('hey jiya')
+# print("""Hello 
+#       World
+#       welcome
+#       kkkk""")
+# a=10
+# print(a)
+
+# a=input("Enter a number:")
+
+# b=input("Enter a number:")
+# c=a+b
+# print(c)
+
+# a=int(input("Enter a number:"))
+# b=int(input("Enter a number:"))
+# c=a+b
+# print(c)
+
+
+# a=int(input("Enter a number:"))
+# b=int(input("Enter a number:"))
+# c=a*b
+# print(c)
+
+# a=int(input("Enter a number:"))
+# b=int(input("Enter a number:"))
+# c=a-b
+# print(c)
+
+# a=int(input("Enter a number:"))
+# b=int(input("Enter a number:"))
+# c=a/b
+# print(c)
+
+
+# a=int(input("Enter a number:"))
+# b=int(input("Enter a number:"))
+# c=a%b
+# print(c)
+
+# a=(input("Enter a number:"))
+# a=int(a)
+# b=(input("Enter a number:"))
+# b=int(b)
+# c=a+b
+# print(c)
+
+# a=(input("Enter a number:"))
+# a=int(a)
+# b=int(input("Enter a number:"))
+# b=int(b)
+# c=a//b
+# print(c)
+
+#swapping
+# a=5
+# b=10
+# t=a
+# a=b
+# b=a
+# print(a)
+# print(b)
+
+
+# a=5
+# b=10
+
+# a=a+b
+# b=a-b
+# a=a-b
+# print(a)
+# print(b)
+
+# a,b=b,a
+# print(a)
+# print(b)
+
+#variables(1st koduthatth varrillaa 2nd  value ayyirkkum priority)
+# a=10
+# a=20
+# print(a)
+
+
+# #type arriyan
+# a=10
+# print(type(a))
+
+# a=10.2
+# print(type(a))
+
+# a=10+5j
+# print(type(a))
+# a='hello'
+# print(type(a))
+
+
+# a=['hello','hyt']
+# print(type(a))
+
+# a=('hello')
+# print(type(a))
+
+# a={'hello'}
+# print(type(a))
+
+# a={'name':'hey u'}    #dictionary
+# print(type(a))
+
+# a=True
+# b=False
+# print(a,b) #boolean
+
+# a=10    # python case senstive aayondd capital 'A' print cheyylla, nammal small leter 'a' aanu value assigne cheytharnnathh
+# b=40
+# c=a+b
+# print(c)
+# print(A)
+# print(b)
+
+
+# myFirstVariable=10
+# print(MyfrstVariable)    
+
+
+# # if statement 
+ 
+# a=int(input("Enter your age:"))
+# if a>18:
+#     print('vote')
+    
+# else:
+#     print("You cannot vote")
+
+
+
+# #odd or even
+
+# a=int(input("Enter a number:"))
+# if a%2:
+#     print('The number is odd')
+    
+# else:
+#     print('The number is even')
+
+
+
+# #elif ladder
+# a=int(input("Enter your age:"))
+# if a>18:
+#     print("you can vote")
+# elif a<18:
+#     print("you cannot vote")
+# else:
+#     print("you can cast your first vote")
+
+
+a=int(input('Enter your number:'))
+b=a%10
+if b==3:
+    print(a+3)
+elif b==5:
+    print(a+5)
+elif b==8:
+    print(a+8)   
+     
+else:
+    print(a+10)
