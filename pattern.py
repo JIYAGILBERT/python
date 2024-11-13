37b75b1d063741fc0f471d0ccd92043b9ffbe0a6
JiyaGilbert <jiyajiya8812@gmail.com>
2024-11-13 16:14:57 +0530

    13 nov

--- pattern.py
+++ pattern.py
@@ -35,16 +35,24 @@
          
         ############## pattern no.:4 ##################
 a=65
+b=1
 for i in range(3):
-    for i in range(3):
+    for j in range(3):
+        print(chr(a+j),end=" ")
+    print()
+    for j in range(3):
+        print(b+j,end=" ")
+    print()
+        
+            
         
          
          
          
-         #####  H/W  #####
+    #      ####  H/W  #####
          
-      #  1) A B C                              2) A B C
-        #   1 2 3                                 c b a
-        #   A B C                                 a b c
-        #   1 2 3                                 c b a
+    #    1) A B C                              2) A B C
+    #       1 2 3                                 c b a
+    #       A B C                                 a b c
+    #       1 2 3                                 c b a
         
